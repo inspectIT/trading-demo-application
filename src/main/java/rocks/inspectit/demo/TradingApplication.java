961b14422da8c6bbb2d934bcc7ec6438f6d11a9a
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-12-21 10:59:19 +0100

    Adding timeout when fetching ETH

--- src/main/java/rocks/inspectit/demo/TradingApplication.java
+++ src/main/java/rocks/inspectit/demo/TradingApplication.java
@@ -4,6 +4,9 @@ import com.github.javafaker.Faker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
 
@@ -25,5 +28,15 @@ public class TradingApplication {
     @PostConstruct
     public void init() {
         log.info("Instance name is: {}", INSTANCE_NAME);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
--- src/main/java/rocks/inspectit/demo/backend/ExchangeController.java
+++ src/main/java/rocks/inspectit/demo/backend/ExchangeController.java
@@ -5,17 +5,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rocks.inspectit.demo.dto.Coin;
 
+import java.util.concurrent.TimeoutException;
+
 @RestController
 public class ExchangeController {
 
     @GetMapping("/price")
-    public double getPrice(@RequestParam Coin coin) throws InterruptedException {
+    public double getPrice(@RequestParam Coin coin) throws InterruptedException, TimeoutException {
         Thread.sleep((long) (Math.random() * 500 + 500));
 
         switch (coin) {
             case BTC:
                 return 61759.90D;
             case ETH:
+                long delay = (long) (Math.random() * 2000 + 4000);
+                if (delay > 5000L) {
+                    Thread.sleep(5000);
+                    throw new TimeoutException("Timeout while fetching price for ETH.");
+                }else {
+                    Thread.sleep(delay);
+                }
                 return 4507.50D;
             default:
                 throw new IllegalArgumentException("Coin '" + coin + "' is not supported.");
