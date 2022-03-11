15a2c17ddb002a7e96950eca3088c4fa5b17843e
Marius Oehler <marius.oehler@novatec-gmbh.de>
2022-03-11 13:36:24 +0100

    Read backend url via spring properties

--- src/main/java/rocks/inspectit/demo/frontend/ExchangeHandler.java
+++ src/main/java/rocks/inspectit/demo/frontend/ExchangeHandler.java
@@ -1,6 +1,7 @@
 package rocks.inspectit.demo.frontend;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -14,31 +15,28 @@ import javax.annotation.PostConstruct;
 @Slf4j
 public class ExchangeHandler {
 
+    private static final String ENV_BACKEND_URL_KEY = "BACKEND_URL";
+
     private static final String DEFAULT_BACKEND_URL = "http://localhost:8080";
 
-    private static final String BACKEND_PROPERTY_KEY = "backend_url";
+    @Value("${" + ENV_BACKEND_URL_KEY + ":" + DEFAULT_BACKEND_URL + "}")
+    private String backendUrl;
 
     @PostConstruct
     public void printBackendUrl() {
-        String backendUrl = getBackendUrl();
         log.info("Using backend URL: {}", backendUrl);
         if (DEFAULT_BACKEND_URL.equals(backendUrl)) {
             log.info("# # # # # # #");
-            log.info("You are using the default backend URL. You can specify a custom URL using the system property '{}'.", backendUrl);
-            log.info("\tExample: java -D{}=http://target:8080 -jar ...", BACKEND_PROPERTY_KEY);
+            log.info("You are using the default backend URL. You can specify a custom URL using the environment variable '{}' or using system properties.", ENV_BACKEND_URL_KEY);
+            log.info("\tExample: java -D{}=http://target:8080 -jar ...", ENV_BACKEND_URL_KEY);
             log.info("# # # # # # #");
         }
-    }
-
-    private String getBackendUrl() {
-        String backendUrl = System.getProperty(BACKEND_PROPERTY_KEY);
-        return backendUrl == null ? DEFAULT_BACKEND_URL : backendUrl;
     }
 
     public Double getPrice(Coin coin) {
         log.info("Get price for {}", coin);
 
-        String target = UriComponentsBuilder.fromHttpUrl(getBackendUrl() + "/price")
+        String target = UriComponentsBuilder.fromHttpUrl(backendUrl + "/price")
                 .queryParam("coin", coin)
                 .encode()
                 .toUriString();
@@ -50,7 +48,7 @@ public class ExchangeHandler {
     public void buy(Quote quote) {
         log.info("Buy {}", quote);
 
-        String target = UriComponentsBuilder.fromHttpUrl(getBackendUrl() + "/trade")
+        String target = UriComponentsBuilder.fromHttpUrl(backendUrl + "/trade")
                 .queryParam("coin", quote.getCoin())
                 .queryParam("price", quote.getPrice())
                 .encode()
